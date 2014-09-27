984285b40bb7f0e46239bba014e21cd3cef8cbdb
coderetrieverblah <code.retriever.blah@gmail.com>
2014-09-27 18:10:08 -0400

    Initial commit

---
+++ InvokeActivityFromAService/src/com/example/invokeactivityfromaservice/MyIntentService.java
@@ -0,0 +1,49 @@
+package com.example.invokeactivityfromaservice;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class MyIntentService extends IntentService 
+{
+
+	public MyIntentService(String name) {
+		super("MyIntentServiceName");
+		// TODO Auto-generated constructor stub
+	}
+	
+	protected void onHandleIntent(Intent intent)
+	{
+		try
+		{
+			int result = downloadFile(new URL("http://www.amazon.com/somefile.pdf"));
+			
+			Intent broadcastIntent = new Intent();
+			broadcastIntent.setAction("FILE_DOWNLOADED_ACTION");
+			getBaseContext().sendBroadcast(broadcastIntent);
+			
+		}catch(MalformedURLException e)
+		{
+			e.printStackTrace();
+		}			
+	}
+	
+	private int downloadFile(URL url)
+	{
+		try
+		{
+			Thread.sleep(5000);
+		}catch(InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return 100;
+	}
+}    
+
---
+++ InvokeActivityFromAService/src/com/example/invokeactivityfromaservice/ServiceActivity.java
@@ -0,0 +1,66 @@
+package com.example.invokeactivityfromaservice;
+
+import com.example.simpleservice.R;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import android.os.Build;
+
+public class ServiceActivity extends Activity {
+	IntentFilter intentFilter;
+	
+	private BroadcastReceiver intentReceiver = new BroadcastReceiver()
+	{
+		@Override
+		public void onReceive(Context context, Intent intent)
+		{
+			Toast.makeText(getBaseContext(), "File downloaded!", Toast.LENGTH_LONG).show();
+		}
+	};
+	
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+	
+		intentFilter = new IntentFilter();
+		intentFilter.addAction("FILE_DOWNLOAD_ACTION");
+		
+		registerReceiver(intentReceiver, intentFilter);
+	}
+    
+	@Override
+	public void onPause()
+	{
+		unregisterReceiver(intentReceiver);
+	}
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+    
+    public void startService(View view)
+    {
+    	startService(new Intent(getBaseContext(), MyIntentService.class));
+    }
+    
+    public void stopService(View view)
+    {
+    	stopService(new Intent(getBaseContext(), MyIntentService.class));
+    }
+}
