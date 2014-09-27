1f7207dfb2b27a402ee8da754ed10ea2be3472cd
coderetrieverblah <code.retriever.blah@gmail.com>
2014-09-27 14:01:49 -0400

    change in package name

--- SimpleAsyncService/src/com/example/simpleasyncservice/MyService.java
+++ SimpleAsyncService/src/com/example/simpleasyncservice/MyService.java
@@ -1,4 +1,4 @@
-package com.example.simpleservice;
+package com.example.simpleasyncservice;
 
 import java.net.MalformedURLException;
 import java.net.URL;
--- SimpleAsyncService/src/com/example/simpleasyncservice/ServiceActivity.java
+++ SimpleAsyncService/src/com/example/simpleasyncservice/ServiceActivity.java
@@ -1,4 +1,6 @@
-package com.example.simpleservice;
+package com.example.simpleasyncservice;
+
+import com.example.simpleservice.R;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
