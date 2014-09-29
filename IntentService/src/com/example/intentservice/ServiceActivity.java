a84cd9d55732934b4c66710d400a1493984022dc
coderetrieverblah <code.retriever.blah@gmail.com>
2014-09-28 21:05:11 -0400

    renaming package

--- IntentService/src/com/example/intentservice/ServiceActivity.java
+++ IntentService/src/com/example/intentservice/ServiceActivity.java
@@ -1,6 +1,6 @@
 package com.example.intentservice;
 
-import com.example.simpleservice.R;
+import com.example.intentservice.R;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
