409ea2edb8174031012a078189ac075297d85e8e
coderetrieverblah <code.retriever.blah@gmail.com>
2014-10-04 22:34:56 -0400

    another commit

--- SaveToStorage/src/com/example/savetostorage/FilesActivity.java
+++ SaveToStorage/src/com/example/savetostorage/FilesActivity.java
@@ -1,5 +1,12 @@
 package com.example.savetostorage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import com.example.savetostorage.R;
 
 import android.support.v7.app.ActionBarActivity;
@@ -37,28 +44,61 @@ public class FilesActivity extends Activity
 		textBox = (EditText) findViewById(R.id.txtText1);
 
 	}
-    
-    public void onClick(View v) {
-        String[] to = 
-            {"someguy@yourcompany.com", 
-        	 "anotherguy@yourcompany.com"};
-        String[] cc = {"busybody@yourcompany.com"};
-        sendEmail(to, cc, "Hello", "Hello my friends!");
-    }
-    
-    //—-sends an SMS message to another device—-
-    private void sendEmail(String[] emailAddresses, String[] carbonCopies,
-    String subject, String message)
-    {
-        Intent emailIntent = new Intent(Intent.ACTION_SEND);
-        emailIntent.setData(Uri.parse("mailto:"));
-        String[] to = emailAddresses;
-        String[] cc = carbonCopies;
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, to);
-        emailIntent.putExtra(Intent.EXTRA_CC, cc);
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
-        emailIntent.setType("message/rfc822");
-        startActivity(Intent.createChooser(emailIntent, "Email"));
-    }    
+ 
+	public void onClickSave(View view) {
+		String str = textBox.getText().toString();
+		try
+		{
+			FileOutputStream fOut = openFileOutput("textfile.txt", MODE_WORLD_READABLE);                        
+			OutputStreamWriter osw = new OutputStreamWriter(fOut);
+
+			//---write the string to the file---
+			osw.write(str);
+			osw.flush(); 
+			osw.close();
+
+			//---display file saved message---
+			Toast.makeText(getBaseContext(), "File saved successfully!", Toast.LENGTH_SHORT).show();
+
+			//---clears the EditText---
+			textBox.setText("");
+		}
+		catch (IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+	}	
+	
+	public void onClickLoad(View view) {
+		try
+		{
+            FileInputStream fIn = new FileInputStream("textfile.txt");
+            InputStreamReader isr = new InputStreamReader(fIn);
+            
+			char[] inputBuffer = new char[READ_BLOCK_SIZE];
+			String s = "";
+
+			int charRead;
+			while ((charRead = isr.read(inputBuffer))>0)
+			{
+				//---convert the chars to a String---
+				String readString =
+						String.copyValueOf(inputBuffer, 0,
+								charRead);
+				s += readString;
+
+				inputBuffer = new char[READ_BLOCK_SIZE];
+			}
+			//---set the EditText to the text that has been 
+			// read---
+			textBox.setText(s);
+
+			Toast.makeText(getBaseContext(),
+					"File loaded successfully!",
+					Toast.LENGTH_SHORT).show();
+		}
+		catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}	
 }
